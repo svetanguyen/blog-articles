0ee22bb1ccae5dae907bc468f707b30f18999748
svetanguyen <matematicaforeber@gmail.com>
2023-02-14 12:53:08 -0500

    add types to themedefault

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,5 +3,12 @@ import { ThemeType } from './theme';
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {
     background: string
+    bannerBg: string
+    primary: string
+    secondary: string
+    textPrimary: string
+    textSecondary: string
+    headerBg: string
+    borderColor: string
   }
 }
